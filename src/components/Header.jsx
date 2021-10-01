720a6c8640012285b9259877b6d4c19d5e4bfef5
victoria_matush <victoria603111@gmail.com>
2021-10-01 20:51:59 +0300

    First commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,6 @@
+const Header = () => {
+    return<header className={'header'}>
+        <img src = 'https://w7.pngwing.com/pngs/146/932/png-transparent-icon-design-illustration-r-alphabet-business-company-logo-free-logo-design-template-company-text-thumbnail.png'/>
+    </header>
+}
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,27 @@
+const Profile = () => {
+    return(
+        <div className={'content'}>
+            <div>
+            <img src={'https://images.ctfassets.net/hrltx12pl8hq/7yQR5uJhwEkRfjwMFJ7bUK/dc52a0913e8ff8b5c276177890eb0129/offset_comp_772626-opt.jpg?fit=fill&w=800&h=300'}/>
+            </div>
+            <div>
+                My posts
+                <div>
+                    New post
+                </div>
+                <div>
+                    <div>
+                        Post1
+                    </div>
+                    <div>
+                        Post2
+                    </div>
+                    <div>
+                        Post3
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default Profile;
