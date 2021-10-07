d60389b41ccb7cd10ef762c30d2f24d84be79e97
victoria_matush <victoria603111@gmail.com>
2021-10-07 10:48:15 +0300

    Add css modules for Navbar, Profile, Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,7 @@
+import s from './Header.module.css'
+
 const Header = () => {
-    return<header className={'header'}>
+    return<header className={s.header}>
         <img src = 'https://w7.pngwing.com/pngs/146/932/png-transparent-icon-design-illustration-r-alphabet-business-company-logo-free-logo-design-template-company-text-thumbnail.png'/>
     </header>
 }
