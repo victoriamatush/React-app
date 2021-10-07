4d33be27e4b40dae2a878270c1cefccffe896abb
victoria_matush <victoria603111@gmail.com>
2021-10-07 12:38:14 +0300

    Add component MyPosts, Post. Add new directories and sorted componets there

---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,14 @@
+import s from './Profile.module.css'
+import Post from "./MyPosts/Post/Post";
+import MyPosts from "./MyPosts/MyPosts";
+const Profile = () => {
+    return(
+        <div className={s.content}>
+            <div>
+            <img src={'https://images.ctfassets.net/hrltx12pl8hq/7yQR5uJhwEkRfjwMFJ7bUK/dc52a0913e8ff8b5c276177890eb0129/offset_comp_772626-opt.jpg?fit=fill&w=800&h=300'}/>
+            </div>
+            <MyPosts/>
+        </div>
+    )
+}
+export default Profile;
