04fd4f5b25f9e9da6675a239e84d95a5697b9435
victoria_matush <victoria603111@gmail.com>
2021-10-14 22:53:49 +0300

    Add Dialog component

---
+++ src/components/Dialogs/Dialogs.jsx
@@ -0,0 +1,10 @@
+import s from './Dialogs.module.css'
+
+const Dialogs = (props) => {
+  return(
+      <div>
+            Dialogs
+      </div>
+  )
+}
+export default Dialogs;
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,11 +1,9 @@
-import s from './Profile.module.css'
-import Post from "./MyPosts/Post/Post";
 import MyPosts from "./MyPosts/MyPosts";
 const Profile = () => {
     return(
-        <div className={s.content}>
+        <div>
             <div>
-            <img src={'https://images.ctfassets.net/hrltx12pl8hq/7yQR5uJhwEkRfjwMFJ7bUK/dc52a0913e8ff8b5c276177890eb0129/offset_comp_772626-opt.jpg?fit=fill&w=800&h=300'}/>
+            <img width={'100%'} src={'https://images.ctfassets.net/hrltx12pl8hq/7yQR5uJhwEkRfjwMFJ7bUK/dc52a0913e8ff8b5c276177890eb0129/offset_comp_772626-opt.jpg?fit=fill&w=800&h=300'}/>
             </div>
             <MyPosts/>
         </div>
