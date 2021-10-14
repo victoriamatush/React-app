8ab1d75f44ba540c34aa7ef562aa59c80b788a65
victoria_matush <victoria603111@gmail.com>
2021-10-14 23:40:52 +0300

    Add routing

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,14 +1,25 @@
 import s from './Navbar.module.css'
+import {NavLink} from "react-router-dom";
 
 const Navbar = () => {
     return(
         <nav className={s.nav}>
-        <div className={`${s.item} ${s.active}`}><a>Profile</a></div>
-        <div className={s.item}><a>Messages</a></div>
-        <div className={s.item}><a>News</a></div>
-        <div className={s.item}><a>Music</a></div>
-        <div className={s.item}><a>Settings</a></div>
-    </nav>
+            <div className={`${s.item} ${s.active}`}>
+                <NavLink to={'/Profile'} activeClassName={s.active}>Profile</NavLink>
+            </div>
+            <div className={s.item}>
+                <NavLink to={'/Dialogs'} activeClassName={s.active}>Messages</NavLink>
+            </div>
+            <div className={s.item}>
+                <NavLink to={'/News'} activeClassName={s.active}>News</NavLink>
+            </div>
+            <div className={s.item}>
+                <NavLink to={'/Music'} activeClassName={s.active}>Music</NavLink>
+            </div>
+            <div className={s.item}>
+                <NavLink to={'/Settings'} activeClassName={s.active}>Settings</NavLink>
+            </div>
+        </nav>
     )
 }
 export default Navbar;
